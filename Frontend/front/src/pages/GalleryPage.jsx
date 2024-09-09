f0220285a5cdbf03c1fab9e2c051bccd9b3aa4c8
임광영 <kokoz22@naver.com>
2024-09-09 15:15:06 +0900

    feat: TripPage와 GalleryPage, NotfoundPage 라우팅 추가

--- Frontend/front/src/pages/GalleryPage.jsx
+++ Frontend/front/src/pages/GalleryPage.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState, useRef } from 'react';
+import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 
 const GalleryPage = () => {
-
+  // const params = useParams();
+  return <h1>Gallery</h1>
 }
 
 
