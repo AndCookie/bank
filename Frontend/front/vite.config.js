a5f6383a70c22dfa639fbc5b2c405d64ebb35beb
정태완 <oodeng98@naver.com>
2024-09-24 16:05:59 +0900

    setting: vite.config.js 수정

--- Frontend/front/vite.config.js
+++ Frontend/front/vite.config.js
@@ -2,12 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"), // @를 src 폴더로 매핑
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      port: 5173,
     },
   },
 });
