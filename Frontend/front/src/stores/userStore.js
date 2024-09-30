e5ffb0a01e7a0b4c9408d7c03acde41b6979a216
임광영 <kokoz22@naver.com>
2024-09-30 14:51:51 +0900

    fix: login 기능

--- Frontend/front/src/stores/userStore.js
+++ Frontend/front/src/stores/userStore.js
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import axiosInstance from '@/axios';
 
 export const useUserStore = create((set) => ({
   // 토큰
@@ -16,17 +17,20 @@ export const useUserStore = create((set) => ({
   // 토큰 발급
   fetchToken: async () => {
     try {
-      const eventSourceToken = new EventSource('https://j11a204.p.ssafy.io/api/accounts/kakao_login_success/');
-      eventSourceToken.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        useUserStore.getState().setUserToken(data);
-        eventSourceToken.close();
-      };
+      const response = await axiosInstance.get('/accounts/login/')
+      console.log(response)
+      
+      // const eventSourceToken = new EventSource('https://j11a204.p.ssafy.io/api/accounts/kakao_login_success/');
+      // eventSourceToken.onmessage = (event) => {
+      //   const data = JSON.parse(event.data);
+      //   useUserStore.getState().setUserToken(data);
+      //   eventSourceToken.close();
+      // };
 
-      eventSourceToken.onerror = (error) => {
-        console.error(error);
-        eventSourceToken.close();
-      };
+      // eventSourceToken.onerror = (error) => {
+      //   console.error(error);
+      //   eventSourceToken.close();
+      // };
     } catch (error) {
       console.log(error);
     }
