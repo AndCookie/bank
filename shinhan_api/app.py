2c161ae9a9138fef0a460e34cb98a638917c64cd
정태완 <oodeng98@naver.com>
2024-10-07 09:05:42 +0900

    fix: ssafy_api app 라이브러리 수정

--- shinhan_api/app.py
+++ shinhan_api/app.py
@@ -5,24 +5,25 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from shinhan_api.request import post
 
 
-def issued_api_key():
+def issued_api_key(email):
     """
     앱 API KEY 발급
     """
     url = "edu/app/issuedApiKey/"
-    body = {"managerId": "jamie9@naver.com"}
+    body = {"managerId": email}
     return post(url, body)
 
 
-def re_issued_api_key():
+def re_issued_api_key(email):
     """
     앱 API KEY 재발급
     """
     url = "edu/app/reIssuedApiKey/"
-    body = {"managerId": "oodeng98@naver.com"}
+    body = {"managerId": email}
     return post(url, body)
     
 
 if __name__ == "__main__":
-    issued_api_key()
-    # print(re_issued_api_key())
+    email = "jamie9@naver.com"
+    # print(issued_api_key(email))
+    print(re_issued_api_key(email))
