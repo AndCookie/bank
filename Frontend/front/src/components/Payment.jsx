3386f16057c77403c79ddc8417d1fc0eee290c67
AndCookie <jamie9@naver.com>
2024-10-08 13:43:47 +0900

    Design: 1009 Payment

--- Frontend/front/src/components/Payment.jsx
+++ Frontend/front/src/components/Payment.jsx
@@ -7,6 +7,7 @@ import { usePaymentStore } from '@/stores/paymentStore';
 import styles from './styles/Payment.module.css';
 import OngoingModal from '@/components/OngoingModal';
 import AdjustModal from '@/components/AdjustModal';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 
 import Checkbox from '@mui/material/Checkbox';
 
@@ -141,15 +142,37 @@ const Payment = ({ selectedDate }) => {
       </div>
 
       {/* 결제 내역 */}
-      {filteredPayments.map((data) => (
-        <div key={data.id} className="d-flex">
-          <div onClick={() => openOngoingModal(data.id)}>{data.pay_date} {data.amount} {data.username}</div>
-          <div>{data.username === userInfo.nickName && <Checkbox checked={data.checked} onChange={() => handleCheck(data.id, data.amount)} />}</div>
-
+      <div className={styles.payContainer}>
+        {filteredPayments.map((data) => (
+          <div key={data.id} className={styles.payContent}>
+            <div className={styles.pay} onClick={() => openOngoingModal(data.id)}>
+              <div className={styles.categoryArea}>
+                {data.category}
+              </div>
+              <div className={styles.costArea}>
+                {data.amount}
+              </div>
+              <div className={styles.dateArea}>
+                {data.pay_date}
+              </div>
+            </div>
+            <div className={styles.checkArea}>
+              {data.username === userInfo.nickName && <Checkbox checked={data.checked} onChange={() => handleCheck(data.id, data.amount)} />}
+            </div>
+          </div>
+        ))}
+      </div>
+      
+      <div className={styles.adjustContainer}>
+        <div className={styles.adjustBackground}>
+          <button className={styles.adjustBtn} onClick={openAdjustModal}>
+            {totalAmount}원 정산하기
+          </button>
+          {/* <button className="slidBtn">
+            <ChevronRightIcon />
+          </button> */}
         </div>
-      ))}
-
-      <button onClick={openAdjustModal}>{totalAmount}원 정산하기</button>
+      </div>
 
       {/* 결제내역 상세 정보 모달 창 */}
       <OngoingModal isOpen={isOngoingOpen} onClose={closeOngoingModal} paymentId={selectedPaymentId} />
